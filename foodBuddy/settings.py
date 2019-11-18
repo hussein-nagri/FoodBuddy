a0dd48b7fc2a54f16aaf88c9382f18a8f6ac493f
Hussein <hznagri@uwaterloo.ca>
2019-11-18 14:13:37 -0500

    welcome page

--- foodBuddy/settings.py
+++ foodBuddy/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'foodBuddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['foodBuddy/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
